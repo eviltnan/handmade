f95e5b7137a89acbcf161087eeb6722b7766eb61
Sergey Cheparev <sergei.cheparev@gmail.com>
2015-09-02 16:55:37 +0200

    Fixed exec paths

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     packages=find_packages(),
     entry_points={
         'console_scripts': [
-            'handmake = handmade.bin:handmake',
+            'handmake = handmade.cli:main',
         ]
     },
 
