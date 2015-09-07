58bf72c296ef8d2aed3f4fccfff245f6f23a315a
Sergey Cheparev <sergei.cheparev@gmail.com>
2015-09-07 18:22:48 +0200

    Temporal import of invoke collections of core plugin

--- handmade/tasks.py
+++ handmade/tasks.py
@@ -1,6 +1,7 @@
 from invoke import ctask as task
 from invoke import Collection
 from plugins.project import tasks as project_tasks
+from plugins.core import tasks as core_tasks
 
 
 @task(default=True)
@@ -10,3 +11,4 @@ def probe(c):
 
 ns = Collection(probe)
 ns.add_collection(Collection.from_module(project_tasks, name='project'))
+ns.add_collection(Collection.from_module(core_tasks, name='core'))
