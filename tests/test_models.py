0f75d886b1eadf2d238e75dc7a2c4bb1ed34dbe0
Sergey Cheparev <sergei.cheparev@gmail.com>
2015-09-07 17:37:03 +0200

    Conftest and test failing test for storages

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,2 +1,11 @@
-def test_mock():
+from handmade.models import BaseModel
+from handmade.models.storages import ModelStorage
+
+
+class TestModel(BaseModel):
     pass
+
+
+def test_model_storage_no_file():
+    storage = ModelStorage(TestModel)
+    storage.read()
