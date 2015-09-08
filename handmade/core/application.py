70969064005ee03943607c4d69acc5168aee5b79
Sergey Cheparev <sergei.cheparev@gmail.com>
2015-09-08 15:20:40 +0200

    Root widget setting

--- handmade/core/application.py
+++ handmade/core/application.py
@@ -2,4 +2,9 @@ from kivy.app import App
 
 
 class HandmadeApplication(App):
-    pass
+    def build(self):
+        import importlib
+        from handmade.conf import settings
+        module, klass = settings.ROOT_WIDGET.rsplit(".", 1)
+        klass = getattr(importlib.import_module(module), klass)
+        return klass()
