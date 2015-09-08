98ca4d3058efb79b4c0268d3ccd2fadf48907a5e
Sergey Cheparev <sergei.cheparev@gmail.com>
2015-09-08 13:17:45 +0200

    Shell and reinstall handmade task for core

--- handmade/plugins/core/tasks.py
+++ handmade/plugins/core/tasks.py
@@ -1,8 +1,24 @@
 from invoke import ctask as task
+from dispatcher import events
+
+
+@task
+def reinstall_handmade(c):
+    # todo: fix this hardcode
+    c.run('cd ../handmade; python setup.py install')
 
 
 @task
 def run(c):
+    events.dispatch('on_initialization')
     from application import HandmadeApplication
     app = HandmadeApplication()
     app.run()
+
+
+# todo: reinstall should be something like setting
+@task(pre=[reinstall_handmade])
+def shell(c):
+    events.dispatch('on_initialization')
+    from IPython import start_ipython
+    start_ipython([])
