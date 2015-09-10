62413c0ddf37bfcee64b9c781e4709b5592063a4
Sergey Cheparev <sergei.cheparev@gmail.com>
2015-09-10 15:59:01 +0200

    Base model interfaces sketch

--- handmade/models/__init__.py
+++ handmade/models/__init__.py
@@ -1,5 +1,15 @@
+from kivy.event import EventDispatcher
 from kivy.properties import NumericProperty
 
 
-class BaseModel(object):
-    id = NumericProperty()
+class BaseModel(EventDispatcher):
+    id = NumericProperty(None, allownone=True)
+    storage = None
+
+    def save(self):
+        raise NotImplementedError("Save is not implemented in base model")
+
+    def delete(self):
+        raise NotImplementedError("Delete is not implemented in base model")
+
+# class JsonStorageModel(BaseModel):
