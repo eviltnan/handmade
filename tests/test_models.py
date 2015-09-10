80f0447b62d2c08faa00e725a3c14a079b840abc
Sergey Cheparev <sergei.cheparev@gmail.com>
2015-09-10 15:58:39 +0200

    Test create model instance in storage

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,11 +1,20 @@
+from kivy.properties import NumericProperty
 from handmade.models import BaseModel
-from handmade.models.storages import ModelStorage
+from handmade.models.storages import JsonModelStorage
 
 
 class TestModel(BaseModel):
-    pass
+    foo = NumericProperty()
 
 
-def test_model_storage_no_file():
-    storage = ModelStorage(TestModel)
-    storage.read()
+def test_json_storage_filename():
+    storage = JsonModelStorage(TestModel)
+    assert storage.filename == 'models/test_model.json'
+
+
+def test_json_storage_create():
+    storage = JsonModelStorage(TestModel)
+
+    instance = TestModel(foo=1)
+    storage.create(instance)
+    assert instance.id == 1, "Instance didn't get id after saving in json storage"
