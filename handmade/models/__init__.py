5b0d77906405fcc60b444e09b85ab3d757e05cfb
Sergey Cheparev <sergei.cheparev@gmail.com>
2015-09-10 17:31:09 +0200

    DoesNotExist for model classes

--- handmade/models/__init__.py
+++ handmade/models/__init__.py
@@ -2,7 +2,19 @@ from kivy.event import EventDispatcher
 from kivy.properties import NumericProperty
 
 
+class BaseModelMeta(type):
+    def __new__(cls, *args, **kwargs):
+        super_new = super(BaseModelMeta, cls).__new__(cls, *args, **kwargs)
+
+        class DoesNotExist(getattr(super_new, 'DoesNotExist', Exception)):
+            pass
+
+        super_new.DoesNotExist = DoesNotExist
+        return super_new
+
+
 class BaseModel(EventDispatcher):
+    __metaclass__ = BaseModelMeta
     id = NumericProperty(None, allownone=True)
     storage = None
 
