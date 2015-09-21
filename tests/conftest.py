57133f5b39dde48852529311b2c4259490032ad8
Sergey Cheparev <sergei.cheparev@gmail.com>
2015-09-21 16:05:39 +0200

    Fixed settings utilization after test finished

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,16 @@
 from _pytest.python import fixture
+from handmade.utils import empty
 
 
 @fixture(autouse=True)
-def settings():
+def settings(request):
     from handmade.conf import settings
     settings.configure(
         STORAGE_ROOT='tests/test_data/storage'
     )
+
+    def fin():
+        settings._wrapped = empty
+
+    request.addfinalizer(fin)
+    return settings
