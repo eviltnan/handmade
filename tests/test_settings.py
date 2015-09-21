691f870f7ce03421671c3d5e24dde48f5cf3b048
Sergey Cheparev <sergei.cheparev@gmail.com>
2015-09-21 16:08:16 +0200

    Fixed settings test

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -2,13 +2,16 @@ import pytest
 
 from handmade.exceptions import ImproperlyConfigured
 from handmade.conf import settings
+from handmade.utils import empty
 
 
-def test_improperly_configured():
+def test_improperly_configured(settings):
+    settings._wrapped = empty
     with pytest.raises(ImproperlyConfigured):
         print settings
 
 
 def test_configure():
+    settings._wrapped = empty
     settings.configure()
     assert not settings.DEBUG
