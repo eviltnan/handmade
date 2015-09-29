45cf74194ea738f6058dda7711e4336e7ab1f4a0
Sergey Cheparev <sergei.cheparev@gmail.com>
2015-09-29 16:49:48 +0200

    Resources list task sketch

--- handmade/resources/tasks.py
+++ handmade/resources/tasks.py
@@ -3,4 +3,9 @@ from invoke import ctask as task
 
 @task
 def list(c):
-    raise NotImplementedError()
+    from handmade.resources import ResourceManager
+    from handmade.conf import settings
+    for plugin in settings.PLUGINS:
+        print "%s:\n" % plugin
+        for type_key, manager in ResourceManager.managers.items():
+            print "%s: %s" % (type_key, manager.registry[plugin].keys())
