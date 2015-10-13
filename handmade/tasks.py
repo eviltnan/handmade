7971c13e0d2fd6f6b28c768a36a1d13470fff998
Sergey Cheparev <sergei.cheparev@gmail.com>
2015-10-13 17:39:27 +0200

    Some startup initialization to plugin discovery

--- handmade/tasks.py
+++ handmade/tasks.py
@@ -1,19 +1,18 @@
 import os
-import sys
 
-os.environ.setdefault("HANDMADE_SETTINGS_MODULE", "settings")
 # hack for kivy not being run
 os.environ["KIVY_NO_ARGS"] = '1'
 from kivy.config import Config
 
 Config.set('kivy', 'log_level', 'debug')
 
-sys.path += [os.getcwd()]
 from invoke import Collection
-from handmade.conf import settings
+
 from handmade.plugins import discover, Plugin
 
 discover()
+from handmade.conf import settings
+
 ns = Collection.from_module(Plugin.plugins['handmade.core'].tasks, name='core')
 
 for plugin_name in settings.PLUGINS:
